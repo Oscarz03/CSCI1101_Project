0627abb72ca994e95f648fb44bdbe3526cdd7489
Oscarz03 <oscarzuniga2003@gmail.com>
2022-04-20 16:37:56 -0500

    Add databases project

---
+++ Games-Database/sanbox-create-update-delete-queries.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+	sandbox(int16_value, double_value, bool_value)
+VALUES
+	(304, 428.21547, 1)
+
+UPDATE
+	sandbox
+SET 
+	int64_value = -1894984113168,
+	string_value = ':joy:'
+WHERE 
+	id = 16
+
+DELETE FROM sandbox WHERE id = 17
